e750a1fcc347c4aecd2238e7dce20d1dbaa17850
Starinin Andrey <starininandrey@gmail.com>
2020-11-06 14:26:14 +0300

    Инициализация проекта

---
+++ TerminalEmulator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TerminalEmulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("*!!! Terminal VIVT !!!*");
+            Console.WriteLine("***********************");
+        }
+    }
+}
