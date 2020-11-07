2d5d767ef312ec0927c4bb446ceb6c11137362f9
Евгений Дулевский <naitmer2003@mail.ru>
2020-11-07 12:33:28 +0300

    Создал структуру программы

--- TerminalEmulator/Program.cs
+++ TerminalEmulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace TerminalEmulator
 {
@@ -6,7 +7,56 @@ namespace TerminalEmulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Write("Terminal>"); string msg = ReadLine().ToUpper();
+                switch (msg)
+                {
+                    case "DIR":
+
+                        break;
+
+                    case "MOVETO":
+
+                        break;
+
+                    case "MADE":
+
+                        break;
+
+                    case "COPY":
+
+                        break;
+
+                    case "DEL":
+
+                        break;
+
+                    case "READ":
+
+                        break;
+
+                    case "CLS":
+                        Clear();
+                        break;
+
+                    case "HELP":
+                        WriteLine("Для получения сведений об определенной команде наберите HELP <имя команды>");
+                        WriteLine("CLS                  Очистка экрана");
+                        WriteLine("COPY                 Копирование текстового файла в указанный каталог\n");
+                        WriteLine("DEL                  Удаление текстового файла");
+                        WriteLine("DIV                  Выводит список файлов и подкаталогов в текущем каталоге\n");
+                        WriteLine("EXIT                 Завершает программу\n");
+                        WriteLine("HELP                 Выводит справочную информацию о командах\n");
+                        WriteLine("MADE                 Создание текстового файла");
+                        WriteLine("MOVETO               Переход в указанный каталог\n");
+                        WriteLine("READ                 Выводит содержимое текстового файла\n");
+                        break;
+
+                    case "EXIT":
+                        return;
+                }
+            }
         }
     }
 }
