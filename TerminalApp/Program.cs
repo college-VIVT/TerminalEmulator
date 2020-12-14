192b53eb369107d2a79870e70e63748cb0a00852
2V01 <74354169+2V01@users.noreply.github.com>
2020-12-14 18:16:49 +0300

    Add files via upload

---
+++ TerminalApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace TerminalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terminal terminal = new Terminal();
+            terminal.start();
+        }
+    }
+}
